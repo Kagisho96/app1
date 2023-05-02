0a487535207979d020dc6e5099428479ac7b062a
Kagisho96 <kagishopitsi@gmail.com>
2023-05-02 18:07:34 +0200

    CHANGED THE TITTLE TO PITSI

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import StartPoint from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <StartPoint tittle={"PITSI"} description={"Mr Mitch ke star, WHEN I GROW U I WANT TO ME MITCH "}/>
   </React.StrictMode>,
 )
