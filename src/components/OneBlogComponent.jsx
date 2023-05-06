0c087a82ad56e9bd836ad18119944cbffbbd4742
Kagisho96 <kagishopitsi@gmail.com>
2023-05-06 11:04:34 +0200

    declared blogPost

--- src/components/OneBlogComponent.jsx
+++ src/components/OneBlogComponent.jsx
@@ -1,10 +1,16 @@
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom"   
+import { blogPosts } from "./BlogComponent";
 
 export const OneBlogComponent = () => {
   const { id} = useParams()
+  const blogPosts = blogPosts.find((blog) => blog.id === parseInt(id)) 
   return (
     <div>
-      <h1> {id}</h1>
+      <h1>{id}</h1>
+      <h3>{blogPosts.title}</h3>
+      <img src={blogPosts.image} alt={blogPosts.title} />
+      <p>{blogPosts.description}</p>
+      <p>{blogPosts.date}</p>
     </div>
   );
 };
