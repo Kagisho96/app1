3c7e0668cc8bb39382597e52a957cc9bb55cdd72
kagisho96 <kagishopitsi@gmail.com>
2023-05-12 12:34:42 +0200

    AddbBolgComponent

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -1,21 +1,22 @@
 import { Link } from "react-router-dom";
 import React, { useState, useEffect } from "react";
-import {initializeApp } from "firebase/app"
-import {getFirestore, collection, addDoc, onSnapshot} from "firebase/firestore"
+import { db } from "./Firebase"
+// import {initializeApp } from "firebase/app"
+import { collection, addDoc, onSnapshot} from "firebase/firestore"
 
 
-// Firebase configurations
-const firebaseConfig = {
-  apiKey: "AIzaSyCNcqogtkV2d3EKES9Czzl4bB486s3jStw",
-  authDomain: "agritech-app-70ee6.firebaseapp.com",
-  projectId: "agritech-app-70ee6",
-  storageBucket: "agritech-app-70ee6.appspot.com",
-  messagingSenderId: "221466055006",
-  appId: "1:221466055006:web:9672d714626a58a7f2bfcf"
-};
+// // Firebase configurations
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCNcqogtkV2d3EKES9Czzl4bB486s3jStw",
+//   authDomain: "agritech-app-70ee6.firebaseapp.com",
+//   projectId: "agritech-app-70ee6",
+//   storageBucket: "agritech-app-70ee6.appspot.com",
+//   messagingSenderId: "221466055006",
+//   appId: "1:221466055006:web:9672d714626a58a7f2bfcf"
+// };
 
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+// const app = initializeApp(firebaseConfig);
+// const db = getFirestore(app);
 
 
 
