37c3f6f883c635ae56af07dd80cf72d2a2341be4
kagisho96 <kagishopitsi@gmail.com>
2023-05-17 11:03:01 +0200

    feat: add ProgressList component

--- src/components/AddBlogPost.jsx
+++ src/components/AddBlogPost.jsx
@@ -67,3 +67,108 @@ const postBlog = (event) => {
 </form>
   </>
 }
+
+
+
+// import { Link } from "react-router-dom";
+// import React, { useState } from "react";
+// // import { blogPosts } from "./BlogComponent";
+// import { db } from "./Firebase"
+// import "firebase/database";
+
+
+// export const AddBlogPost = () => {
+//   const [postData, setPostData] = useState({
+//     title: "",
+//     description: "",
+//     body: []
+//   });
+
+//   const formatText = (text) => {
+//     return text.split(",");
+//   };
+
+//   const handleChange = (event) => {
+//     event.preventDefault();
+//     const { name, value } = event.target;
+//     setPostData({
+//       ...postData,
+//       [name]: name === "body" ? formatText(value) : value
+//     });
+//   };
+
+//   // const postBlog = (event) => {
+//   //   event.preventDefault();
+//   //   database.ref("blogPosts").push(postData, (error) => {
+//   //     if (error) {
+//   //       console.log("Error posting blog:", error);
+//   //     } else {
+//   //       console.log("Blog post added successfully!");
+//   //     }
+//   //   });
+//   //   setPostData({
+//   //     title: "",
+//   //     description: "",
+//   //     body: []
+//   //   });
+//   // };
+
+//   return (
+//     <>
+//       <Link to={`/blok`}></Link>
+//       <form
+//         className="max-w-2xl mx-auto mt-6 p-4 bg-white rounded-lg shadow-md"
+//         onChange={handleChange}
+//         onSubmit={postBlog}
+//       >
+//         <div className="mb-4">
+//           <label className="block text-gray-700 font-bold mb-2" htmlFor="title">
+//             Blog title
+//           </label>
+//           <input
+//             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+//             type="text"
+//             id="title"
+//             name="title"
+//             placeholder="Title"
+//             value={postData.title}
+//           />
+//         </div>
+
+//         <div className="mb-4">
+//           <label className="block text-gray-700 font-bold mb-2" htmlFor="description">
+//             Blog description
+//           </label>
+//           <textarea
+//             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+//             id="description"
+//             name="description"
+//             placeholder="Description"
+//             value={postData.description}
+//           ></textarea>
+//         </div>
+
+//         <div className="mb-4">
+//           <label className="block text-gray-700 font-bold mb-2" htmlFor="body">
+//             Blog Body
+//           </label>
+//           <textarea
+//             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+//             id="body"
+//             name="body"
+//             placeholder="Blog post body, separated by commas"
+//             value={postData.body}
+//           ></textarea>
+//         </div>
+
+//         <div className="flex items-center justify-between">
+//           <input
+//             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+//             type="submit"
+//             value="Post Blog"
+//           />
+//         </div>
+//       </form>
+//     </>
+//   );
+// };
